bc200523053cfd66262e4fc12b71d0b445f2348b
Elliot Powell <johnp8727@gmail.com>
2018-11-08 00:42:19 +0000

    moved to PIPE

--- flashing.py
+++ flashing.py
@@ -3,7 +3,7 @@ from os import path
 
 def FlashDFUwin(mcu, file):
     print("Starting flashing...")
-    erase = run(["dfu-programmer", mcu, "erase"], shell=True, capture_output=True)
+    erase = run(["dfu-programmer", mcu, "erase"], shell=True, stderr=PIPE)
     if ("dfu-programmer: no device present") in erase.stderr.decode("utf-8"):
         print("erase failed - Have you pressed the reset button")
         return
@@ -11,12 +11,12 @@ def FlashDFUwin(mcu, file):
         print(erase.stderr.decode("utf-8"))
 
     print("flashing ", file, "to device")
-    flash = run(["dfu-programmer", mcu, "flash", path.abspath(file)], shell=True, capture_output=True)
+    flash = run(["dfu-programmer", mcu, "flash", path.abspath(file)], shell=True, stderr=PIPE)
     #print(flash)
     if flash.returncode == 0:
         print(flash.stderr.decode("utf-8"))
 
     print("flash successful")
     print("resetting device")
-    run(["dfu-programmer", mcu, "reset"], shell=True, capture_output=True)
+    run(["dfu-programmer", mcu, "reset"], shell=True, stderr=PIPE)
     print("reset successful")
