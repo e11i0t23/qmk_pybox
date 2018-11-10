d1b13b0f7674fe2052c1449b0f5416736e781c45
Elliot Powell <johnp8727@gmail.com>
2018-11-10 14:25:28 +0000

    fixed perms

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y python3 python3-pip dfu-programmer
 sudo pip3 install easygui pyinstaller PyQt5
 sudo pyinstaller entry_point.py --onefile --name 'QMK-Pybox'
 cp ./dist/QMK-Pybox ~/Desktop
-cp ./50-QMK-Pybox.rules /etc/udev/rules.d/
+sudo cp ./50-QMK-Pybox.rules /etc/udev/rules.d/
